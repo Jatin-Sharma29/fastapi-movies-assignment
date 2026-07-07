d372a6e42ef504ddf1c64ab8d27d33243b0bc398
Jatin-Sharma29 <sharma.jatin06129@gmail.com>
2026-07-07 22:53:39 +0530

    fastapi-movies-assignment

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel
+
+app=FastAPI()
+
+class MovieUpdate(BaseModel):
+    title:str
+    director:str
+    genre:str
+    language:str
+    release_year:int
+
+movies = [
+ {
+ "id": 1,
+ "title": "3 Idiots",
+ "director": "Rajkumar Hirani",
+ "genre": "Comedy Drama",
+ "language": "Hindi",
+ "release_year": 2009
+ },
+ {
+ "id": 2,
+ "title": "Baahubali",
+ "director": "S S Rajamouli",
+ "genre": "Action Drama",
+ "language": "Telugu",
+ "release_year": 2015
+ }
+]
+
+@app.get("/")
+def home():
+    return {"message":"Movie API is Running"}
+
+@app.get("/movies")
+def get_movie():
+    return movies
+
+@app.put("/movies/{movie_id}")
+def update_movie(movie_id : int , movie : MovieUpdate):
+    for existing_movie in movies:
+        if existing_movie["id"]==movie_id:
+            existing_movie.update(movie.model_dump())
+            return{
+                "message":"Movie updated successfully",
+                "movie":existing_movie
+            }
+    raise HTTPException(status_code=404,detail="Movie not found")
